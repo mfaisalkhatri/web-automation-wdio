4c09c5d54422fb8f52e542dc83a1e66aa42e79a6
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-07-20 00:43:48 +0530

    updated wdio config to run tests in headless mode

--- wdio.conf.js
+++ wdio.conf.js
@@ -54,7 +54,10 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
     },
     // {
     //     browserName: 'firefox'
