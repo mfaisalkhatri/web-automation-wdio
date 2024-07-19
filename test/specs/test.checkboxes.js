0050f8c412f96b777e3ab12b52f8c3c2f06f3ccc
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-07-19 18:44:12 +0530

    added example for checkbox handling

---
+++ test/specs/test.checkboxes.js
@@ -0,0 +1,22 @@
+const { browser } = require("@wdio/globals");
+
+describe("Learning WebdriverIO", () => {
+  it("should tick the checkbox", async () => {
+    await browser.url("https://the-internet.herokuapp.com/checkboxes");
+
+    let pageHeader = await $("h3=Checkboxes");
+    await expect(pageHeader).toHaveText("Checkboxes");
+
+    let checkboxOne = await $("#checkboxes input:first-child");
+    await checkboxOne.click();
+
+    await expect(checkboxOne).toBeSelected();
+  });
+
+  it("should un-tick the checkbox", async () => {
+    let checkboxTwo = await $("#checkboxes input:last-child");
+    await checkboxTwo.click();
+
+    await !expect(checkboxTwo).toBeSelected();
+  });
+});
