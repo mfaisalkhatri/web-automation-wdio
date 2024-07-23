f5d32e00c8590f4c0121180e9a797843024fbc6e
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-07-23 22:03:40 +0530

    updated configuration

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,7 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
         'goog:chromeOptions': {
             args: ['headless', 'disable-gpu']
         }
