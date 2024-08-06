6e44282de48e0b80ef302843bf6e4532ffdd1b10
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2024-08-06 19:52:12 +0530

    Added example tests for mouse hover (#39)
    * added example code to perform mouse hover
    
    * updated config to run tests on chrome and edge only
    
    * updated config to run tests on chrome only
    
    * updated config to run tests on chrome and firefox only

---
+++ test/specs/test.mouse-hover.js
@@ -0,0 +1,16 @@
+const { browser } = require("@wdio/globals");
+
+describe("Learning WebdriverIO - Mouse Hover", () => {
+  it("should perform mouse hover", async () => {
+    await browser.url("https://the-internet.herokuapp.com/hovers");
+
+    let pageHeader = await $("<h3 />");
+    await expect(pageHeader).toHaveText("Hovers");
+
+    let imageOne = await $("#content > div > div:nth-child(3)");
+    await imageOne.moveTo();
+
+    let nameOfImageOne = await $("<h5 />");
+    await expect(nameOfImageOne).toHaveText("name: user1");
+  });
+});
