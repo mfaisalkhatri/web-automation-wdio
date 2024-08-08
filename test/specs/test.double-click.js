6fbe4dd5d1060a37606d29242ad3e2a2b5ee94a8
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-08-08 17:55:56 +0530

    added example code for double click tests

---
+++ test/specs/test.double-click.js
@@ -0,0 +1,14 @@
+const { browser } = require("@wdio/globals");
+
+describe("Learning WebdriverIO - Double click a button", () => {
+  it("should double click on a button", async () => {
+    await browser.url("https://demoqa.com/buttons");
+
+    let doubleClickMeBtn = await $('#doubleClickBtn');
+    await doubleClickMeBtn.scrollIntoView();    
+    await doubleClickMeBtn.doubleClick();
+
+    const message = await $('#doubleClickMessage');
+    await expect(message).toHaveText('You have done a double click');
+  });
+});
